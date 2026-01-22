1b7c9b308f36dcc8fe01dca03866afec9497c614
cblairwhccd <christianblair@my.whccd.edu>
2026-01-21 19:37:58 -0800

    Prompt for input and parse

--- Program.cs
+++ Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        /* Get information */
+        Console.Write("Enter first name: ");
+        string firstName = Console.ReadLine();
+        Console.Write("Enter last name: ");
+        string lastName = Console.ReadLine();
+        Console.Write("Enter number 1: ");
+        string number1 = Console.ReadLine();
+        Console.Write("Enter number 2: ");
+        string number2 = Console.ReadLine();
+
+        /* Parse numerical input (assume valid input) */
+        double n1 = double.Parse(number1);
+        double n2 = double.Parse(number2);
     }
 }
